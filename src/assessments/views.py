7e9000c80a48ebcb25de7a1dd45c93873808ea4f
Vhutali01 <vhutalitsanwani01@gmail.com>
2025-06-25 21:48:22 +0200

    Refactor dental screening view to improve error handling and use get_or_create for screening records -does not create duplicates only updates ore creates a new one

--- src/assessments/views.py
+++ src/assessments/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import DentalScreeningForm
-
+from django.contrib import messages
 from patient.models import Patient
 from .models import DentalScreening
 
@@ -25,54 +24,77 @@ def dental_screening(request, patient_id):
     
     if request.method == 'POST':
 
-        missing = [field for field in required_fields if not request.POST.get(field)]
-        if missing:
-            # Show error and re-render form
-            from django.contrib import messages
-            messages.error(request, f"Please answer all required questions: {', '.join(missing)}")
-            return render(request, 'assessments/dental_screening.html', {
-                'permanent_upper': permanent_upper,
-                'permanent_lower': permanent_lower,
-                'primary_upper': primary_upper,
-                'primary_lower': primary_lower,
-            })
+        try:
 
-        teeth_fields = {}
+            missing = [field for field in required_fields if not request.POST.get(field)]
+            if missing:
+                #show error and re-render form
+                messages.error(request, f"Please answer all required questions: {', '.join(missing)}")
+                return render(request, 'assessments/dental_screening.html', {
+                    'permanent_upper': permanent_upper,
+                    'permanent_lower': permanent_lower,
+                    'primary_upper': primary_upper,
+                    'primary_lower': primary_lower,
+                })
 
-        for tooth in permanent_upper + permanent_lower + primary_upper + primary_lower:
+            teeth_fields = {}
 
-            key = f"tooth_{tooth}"
-            teeth_fields[key] = request.POST.get(key, "")
+            for tooth in permanent_upper + permanent_lower + primary_upper + primary_lower:
 
-        #collect other fields
-        screening = DentalScreening.objects.create(
-            patient=patient,
-            caregiver_treatment=request.POST.get('caregiver_treatment', ''),
-            income=request.POST.get('income', ''),
-            sugar_meals=request.POST.get('sugar_meals', ''),
-            sugar_snacks=request.POST.get('sugar_snacks', ''),
-            sugar_beverages=request.POST.get('sugar_beverages', ''),
-            sa_citizen=request.POST.get('sa_citizen', ''),
-            special_needs=request.POST.get('special_needs', ''),
-            plaque=request.POST.get('plaque', ''),
-            dry_mouth=request.POST.get('dry_mouth', ''),
-            enamel_defects=request.POST.get('enamel_defects', ''),
-            appliance=request.POST.get('appliance', ''),
-            fluoride_water=request.POST.get('fluoride_water', ''),
-            fluoride_toothpaste=request.POST.get('fluoride_toothpaste', ''),
-            topical_fluoride=request.POST.get('topical_fluoride', ''),
-            regular_checkups=request.POST.get('regular_checkups', ''),
-            sealed_pits=request.POST.get('sealed_pits', ''),
-            restorative_procedures=request.POST.get('restorative_procedures', ''),
-            enamel_change=request.POST.get('enamel_change', ''),
-            dentin_discoloration=request.POST.get('dentin_discoloration', ''),
-            white_spot_lesions=request.POST.get('white_spot_lesions', ''),
-            cavitated_lesions=request.POST.get('cavitated_lesions', ''),
-            multiple_restorations=request.POST.get('multiple_restorations', ''),
-            missing_teeth=request.POST.get('missing_teeth', ''),
-            teeth_data=teeth_fields,
-        )
-        return redirect('report', patient_id=patient_id)  #redirects to report page and sends patient_id for identification
+                key = f"tooth_{tooth}"
+                teeth_fields[key] = request.POST.get(key, "")
+
+            #collect other fields
+
+            screening, created = DentalScreening.objects.get_or_create(
+                patient=patient,
+                defaults={
+                'caregiver_treatment': request.POST.get('caregiver_treatment', ''),
+                'income': request.POST.get('income', ''),
+                'sugar_meals': request.POST.get('sugar_meals', ''),
+                'sugar_snacks': request.POST.get('sugar_snacks', ''),
+                'sugar_beverages': request.POST.get('sugar_beverages', ''),
+                'sa_citizen': request.POST.get('sa_citizen', ''),
+                'special_needs': request.POST.get('special_needs', ''),
+                'plaque': request.POST.get('plaque', ''),
+                'dry_mouth': request.POST.get('dry_mouth', ''),
+                'enamel_defects': request.POST.get('enamel_defects', ''),
+                'appliance': request.POST.get('appliance', ''),
+                'fluoride_water': request.POST.get('fluoride_water', ''),
+                'fluoride_toothpaste': request.POST.get('fluoride_toothpaste', ''),
+                'topical_fluoride': request.POST.get('topical_fluoride', ''),
+                'regular_checkups': request.POST.get('regular_checkups', ''),
+                'sealed_pits': request.POST.get('sealed_pits', ''),
+                'restorative_procedures': request.POST.get('restorative_procedures', ''),
+                'enamel_change': request.POST.get('enamel_change', ''),
+                'dentin_discoloration': request.POST.get('dentin_discoloration', ''),
+                'white_spot_lesions': request.POST.get('white_spot_lesions', ''),
+                'cavitated_lesions': request.POST.get('cavitated_lesions', ''),
+                'multiple_restorations': request.POST.get('multiple_restorations', ''),
+                'missing_teeth': request.POST.get('missing_teeth', ''),
+                'teeth_data': teeth_fields,
+            }
+            )
+            
+            if not created:
+
+                fields = [
+                    'caregiver_treatment', 'income', 'sugar_meals', 'sugar_snacks', 'sugar_beverages',
+                    'sa_citizen', 'special_needs', 'plaque', 'dry_mouth', 'enamel_defects', 'appliance',
+                    'fluoride_water', 'fluoride_toothpaste', 'topical_fluoride', 'regular_checkups',
+                    'sealed_pits', 'restorative_procedures', 'enamel_change', 'dentin_discoloration',
+                    'white_spot_lesions', 'cavitated_lesions', 'multiple_restorations', 'missing_teeth'
+                ]
+
+                for field in fields:
+                    setattr(screening, field, request.POST.get(field, ''))
+                screening.teeth_data = teeth_fields
+                screening.save()
+
+            return redirect('report', patient_id=patient_id)  #redirects to report page and sends patient_id for identification
+
+        except Exception as e:
+            messages.error(request, f"An error occurred: {str(e)}")
 
     return render(request, 'assessments/dental_screening.html', {
         'permanent_upper': permanent_upper,
